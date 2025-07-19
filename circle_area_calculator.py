c969c39c4a1caf1308f1bec25036809be6ed9a86
Ben Kelly <ben.kelly436@gmail.com>
2025-07-19 19:43:00 +0100

    updated calculator

--- circle_area_calculator.py
+++ circle_area_calculator.py
@@ -1,21 +1,31 @@
-import math
+from math import pi
+from decimal import Decimal
 
-print("""        Circle area calculator
+print("""\n        Circle area calculator
       ___________________________\n
       """)
 
-while True:
+program_running = True
+pi = Decimal.from_float(pi)
+
+while program_running:
     try:
-        radius = float(input("Enter the circle's radius: "))
-        if radius >= 0:
+        radius = Decimal.from_float(float(input("Enter the circle's radius: ")))
+        decimal_places = int(input("Enter the number of decimal places to round to: "))
+        if radius >= 0 and decimal_places >= 0:
               break
-        else:
-              print("\nThe radius must be a positive real number!\n")
-              continue
+        elif radius < 0: 
+          print("\nThe radius must be a positive real number!\n")
+        elif decimal_places < 0:
+          print("\nThe number of decimal places must be a natural number!\n")
+        continue
     except ValueError:
-        print("\nThe radius must be a positive real number!\n")
+        print("\nYou must enter a number!\n")
 
-area = round(math.pi * (radius)**2, 2)
+if decimal_places > 0:
+  area = round((pi * (radius)**2), decimal_places)
+  
+else: 
+  area = (pi * (radius)**2)
 
-print(f"\nThe area of a circle with radius {radius} is {area}\n")
-
+print(f"\nThe area of a circle with radius {radius} units is {area} units squared\n")
