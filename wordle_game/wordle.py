7d82b845847ae44093ea80b9d05a3a99628a6fba
Ben Kelly <ben.kelly436@gmail.com>
2025-10-21 00:38:10 +0100

    bug fixes: i) the game no longer outputs win and fail messages after successfully guessing the word on the final attempt. ii) fixed issue with misplaced letter feedback when a guess with two of the same letter was made.

--- wordle_game/wordle.py
+++ wordle_game/wordle.py
@@ -6,14 +6,14 @@ with open("word_bank.txt", "r") as file:
     for line in file:
         word_bank.append(line.strip().upper()) # parse file, strip whitespace, convert to uppercase and add each word to word_bank
         
-chosen_word = random.choice(word_bank)
+chosen_word = "ASDFG" #random.choice(word_bank)
 
 misplaced_letters = []
 incorrect_letters = []
 attempts = 6
 current_attempt = 1
 
-def count_letters(word):   # helper function to count occurrences of each letter in a word
+def count_letters(word):   # helper function to count occurrences of each letter in a guessed word
   letter_count = {}
   for letter in word:
     if letter in letter_count:
@@ -35,7 +35,8 @@ RULES:\n
   - You will be told which letters from your guess are in the word but not in the correct position.
   - You will be told which letters from your guess are not in the word.\n""")
 
-while current_attempt <= attempts:   # main game loop
+# main game loop
+while current_attempt <= attempts: 
   
   print(f"\nATTEMPT {current_attempt}")
   
@@ -43,25 +44,44 @@ while current_attempt <= attempts:   # main game loop
   
   print("\n"+guess)
   
-  if len(guess) != 5 or not guess.isalpha():                  # input validation
+  # input validation
+  if len(guess) != 5 or not guess.isalpha():                
     print("Invalid input. Please enter a 5-letter word.\n")
     continue
   
   index = 0
   game_state = "_____" # initialize game state with underscores for unguessed letters
   
+  guess_letters = {} # initialize dictionary to track number of occurrences of each letter. this will be incremented each pass through the loop for a running total to ensure the list of misplaced and incorrect letters remain accurate
+  
+  # process the guess
+  
   for letter in guess:
     
+  # the first if/else of the loop constructs a dictionary which acts as a running total of the number of occurrences of each letter
+  
+    if letter in guess_letters:  
+      guess_letters[letter] += 1
+      
+    else:
+      guess_letters[letter] = 1
+      
+  # conditionals to check for misplaced letters and incorrect letters
+    
+    ## update game state with correctly guessed letter
     if letter == chosen_word[index]:
-      game_state = game_state[:index] + letter + game_state[index+1:] # update game state with correctly guessed letter
+      game_state = game_state[:index] + letter + game_state[index+1:] 
       
+      ## nested if removes letter from misplaced if now correctly placed
       if letter in misplaced_letters:
-        misplaced_letters.remove(letter) # remove letter from misplaced if now correctly placed
-      
-    elif letter in chosen_word and not (misplaced_letters) and (count_letters(guess).get(letter) < 2): # check for misplaced letters while avoiding duplicates
+        misplaced_letters.remove(letter)
+        
+    ## check for misplaced letters while avoiding duplicates
+    elif (letter in chosen_word) and (letter not in misplaced_letters) and (guess_letters[letter] == 1):
       misplaced_letters.append(letter)
       
-    elif letter not in chosen_word and letter not in incorrect_letters:
+    ## check for incorrect letters  
+    elif (letter not in chosen_word) and (letter not in incorrect_letters):
       incorrect_letters.append(letter) # add to incorrect letters if not already present
       
     index += 1
@@ -74,9 +94,12 @@ while current_attempt <= attempts:   # main game loop
     
   current_attempt += 1
   
+  # win condition
   if guess == chosen_word:
     print(f"""\n\U0001F973 \U0001F389 CONGRATULATIONS! You guessed the word '{chosen_word}' correctly! \U0001F973 \U0001F389""")
     break
   
-if current_attempt > attempts:
-  print(f"""\n\U0001F622 \U0001F494 SORRY! You've used all your attempts. The correct word was '{chosen_word}'. \U0001F622 \U0001F494""")
+  # fail condition  
+  if current_attempt > attempts:
+    print(f"""\n\U0001F622 \U0001F494 SORRY! You've used all your attempts. The correct word was '{chosen_word}'. \U0001F622 \U0001F494""")
+    break
